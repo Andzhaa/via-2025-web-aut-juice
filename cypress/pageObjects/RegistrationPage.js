26e8475c961fa76e5c84ec70a7903504377e03f9
Anrians Fedotovs <156031292+Andzhaa@users.noreply.github.com>
2025-04-25 14:06:51 +0300

    aizmirsu pielikt ot jauno failu

---
+++ cypress/pageObjects/RegistrationPage.js
@@ -0,0 +1,35 @@
+import { BasePage } from "./basePage";
+
+export class RegistrationPage extends BasePage {
+    static get url() {
+        return "/#/register";
+    }
+
+    static get emailField() {
+        return cy.get("#emailControl");
+    }
+
+    static get passwordField() {
+        return cy.get("#passwordControl");
+    }
+
+    static get repeatPasswordField() {
+        return cy.get("#repeatPasswordControl");
+    }
+
+    static get securityQuestionField() {
+        return cy.get("[name='securityQuestion']");
+    }
+
+    static get securityQuestionOption() {
+        return cy.get("mat-option[role='option']");
+    }
+
+    static get securityAnswerField() {
+        return cy.get("#securityAnswerControl");
+    }
+
+    static get registrationButton() {
+        return cy.get("button#registerButton")
+    }
+}
